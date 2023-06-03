27c8987477fab31ab65920d6743075246b748627
Mark Wilkes <mcwilkes@gmail.com>
2023-06-03 11:59:13 -0600

    Removed tokyonight colorscheme

--- plugins/community.lua
+++ plugins/community.lua
@@ -5,6 +5,6 @@ return {
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.colorscheme.tokyonight" },
+  -- { import = "astrocommunity.colorscheme.tokyonight" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
